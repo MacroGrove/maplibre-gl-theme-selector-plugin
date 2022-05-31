ce0c9b34fbb7247966c27b24be90b54605b448b0
MacroGrove <71152799+MacroGrove@users.noreply.github.com>
2022-05-30 22:00:23 -0400

    stubbing

---
+++ static/scripts/maplibre-gl-theme-selector-plugin.js
@@ -0,0 +1,23 @@
+class MapLibreThemeSelector {
+
+    constructor(options) {
+        options = Object.assign({}, options);
+
+
+        if (!(this instanceof MapLibreDarkMode)) {
+            throw new Error('Instantiation Error: MapLibreDarkMode needs to be called with the new keyword.');
+        }
+    }
+
+    addSelector() {
+
+    }
+
+    onAdd(map) {
+
+    }
+
+    onRemove() {
+
+    }
+}
